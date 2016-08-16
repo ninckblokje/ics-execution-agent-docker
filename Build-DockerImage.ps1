b89000878094b7053bf9cedb30a692d0ed3c28e6
Jeroen NB <jeroen.ninckblok@gmail.com>
2016-08-16 14:16:47 +0200

    temp files now each as separate volume (command still erroneous)

--- Build-DockerImage.ps1
+++ Build-DockerImage.ps1
@@ -4,12 +4,24 @@ $DockerPwd=$PWD.path
 $DockerPwd=$DockerPwd.Replace('\','/')
 $DockerPwd=$DockerPwd.Substring(0,1).ToLower() + $DockerPwd.Substring(2)
 
-Write-Host Creating Docker image [ninckblokje/icsea:$ImageVersion] from directory [$DockerPwd] 
+Write-Host Creating Docker image [ninckblokje/icsea:$ImageVersion] from directory [$DockerPwd]
+
+$TempFileVolumes=""
+Get-ChildItem "$PWD\temp_files" | 
+Foreach-Object {
+    $TempFile=$_.FullName
+    $TempFile=$TempFile.Replace('\','/')
+    $TempFile=$TempFile.Substring(0,1).ToLower() + $TempFile.Substring(2)
+    
+    $TempName=$_.Name
+    
+    Write-Host Found temp install file [$TempFile]
+    $TempFileVolumes=$TempFileVolumes + " -v /${TempFile}:/tmp/icsea/ICSOPInstall/tmp/$TempName"
+}
 
 Write-Host Building image
 docker build -t ninckblokje/icsea-ph1:$ImageVersion icsea
 
 Write-Host Running install
-#docker run -it -v /$DockerPwd/files:/tmp/files -v /$DockerPwd/temp_files:/tmp/icsea/ICSOPInstall/tmp ninckblokje/icsea-ph1:$ImageVersion /tmp/scripts/install_execution_agent.sh
 
-docker run -it -v /$DockerPwd/files:/tmp/files -v /$DockerPwd/temp_files:/tmp/icsea/ICSOPInstall/tmp ninckblokje/icsea-ph1:$ImageVersion bash
+echo docker run -it -v /$DockerPwd/files:/tmp/files $TempFileVolumes ninckblokje/icsea-ph1:$ImageVersion bash
